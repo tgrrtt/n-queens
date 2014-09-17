e95238dda7f968e1762c29591341a040ea111e86
John Heroy <johnheroy@gmail.com>
2014-09-17 12:20:40 -0700

    Implement check rows and check columns

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,19 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var row = this.get(rowIndex);
+      var count = 0;
+      for (var i = 0; i < row.length; i++){ count += row[i]; }
+      return (count > 1);
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var anyConflicts = false;
+      for (var i = 0; i < this.get('n'); i++){
+        anyConflicts = (anyConflicts || this.hasRowConflictAt(i));
+      }
+      return anyConflicts;
     },
 
 
@@ -94,12 +101,20 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var count = 0;
+      for (var i = 0; i < this.get('n'); i++){
+        count += this.get(i)[colIndex];
+      }
+      return (count > 1);
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      var anyConflicts = false;
+      for (var i = 0; i < this.get('n'); i++){
+        anyConflicts = (anyConflicts || this.hasColConflictAt(i));
+      }
+      return anyConflicts;
     },
 
 
